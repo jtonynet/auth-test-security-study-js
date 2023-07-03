7e61cb2be50823789d753e103d8c2bd5b3b291fd
jtonyNet <jtony.net@gmail.com>
2023-07-03 12:47:44 -0300

    feat(assercao na unha) implementando assercoes simplorias com finalidade didatica

---
+++ packages/01-banco-de-horas/index.js
@@ -0,0 +1,25 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = (valor) => {
+    const assercoes = {
+        ehExatamenteIgualA(esperado) {
+            if (valor !== esperado) {
+                
+            }
+        }
+    };
+}
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
