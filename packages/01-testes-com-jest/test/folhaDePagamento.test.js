c5ba3a89f4b7a57e2cb735d6832f6b015a224925
jtonyNet <jtony.net@gmail.com>
2023-07-04 01:41:30 -0300

    feat(coverage): 100% ou sera que nao?

--- packages/01-testes-com-jest/test/folhaDePagamento.test.js
+++ packages/01-testes-com-jest/test/folhaDePagamento.test.js
@@ -1,18 +1,17 @@
 import { calculaDescontos, somaHorasExtras } from '../index.js';
 
 describe('Testes dos calculos de folha', () => {
-    it('Deve retornar a soma das horas extras', () => {
-        const esperado = 2500;
-        const retornado = somaHorasExtras(2000, 500);
-      
-        expect(retornado).toBe(esperado);
-      });
-      
-      it('Deve descotar o valor do salario', () => {
-        const esperado = 2300;
-        const retornado = calculaDescontos(2500, 200);
-      
-        expect(retornado).toBe(esperado);
-      });
-})
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
+    expect(retornado).toBe(esperado);
+  });
+
+  it('Deve descotar o valor do salario', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+
+    expect(retornado).toBe(esperado);
+  });
+});
